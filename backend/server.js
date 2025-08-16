297f6192cb7e23f0ed085af54ebf93c9861113d3
Raghav <raghav2004kaushal@gmail.com>
2025-08-16 14:05:46 +0530

    Implement server file

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+require("dotenv").config()
+
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port... ${PORT}`);
+});
