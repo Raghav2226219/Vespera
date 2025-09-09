ce5d11d3a2ef14691d0aa24d9b9c0cb5a90ea53d
Raghav <raghav2004kaushal@gmail.com>
2025-09-09 17:40:29 +0530

    Initialize React application and set up project folder structure

---
+++ frontend/vespers/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="h-screen flex items-center justify-center bg-blue-600 text-white text-4xl font-bold">
+      🚀 Tailwind v3 is working!
+    </div>
+    </>
+  )
+}
+
+export default App
